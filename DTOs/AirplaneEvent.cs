2fd1c55d2fedd59c5e7571e45f922f4e9211b643
Vlad Shell <boldingosha@yandex.ru>
2021-04-02 13:16:27 +0300

    Add files via upload

---
+++ DTOs/AirplaneEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoPanel.DTOs
+{
+	public enum EventType
+	{
+		Arrival, // Boarding
+		Departure
+	}
+
+	public class AirplaneEventW
+	{
+		public Guid PlaneId { get; set; }
+
+		public EventType EventType { get; set; }
+
+		public DateTime DepartureTime { get; set; }
+
+		// public int TicketCount { get; set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InfoPanel
+{
+	class Program
+	{
+		const int MIN_PLANE_ARRIVAL_TIME = 20 * 60 * 1000; // прибытие следующего самолета от 20 минут
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
